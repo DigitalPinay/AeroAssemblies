fd569faadfdcad0859b7ac4f6c6964775f189d5e
Therese Peterson <tpeterson.rd@gmail.com>
2018-12-13 16:58:28 -0600

    undo

--- scripts/SplashRollover.js
+++ scripts/SplashRollover.js
@@ -8,14 +8,14 @@ if (document.images) {
     Garage = new Image
     Pop = new Image
 
-    Front.src = "../images/home/front.jpg"
-    Eyelet.src = "../images/home/eyelets.jpg"
-    Fastener.src = "../images/home/fasteners.jpg"
-    Rope.src = "../images/home/rope.jpg"
-    Wire.src = "../images/home/wire.jpg"
-    Security.src = "../images/home/security.jpg"
-    Garage.src = "../images/home/garage.jpg"
-    Pop.src = "../images/home/pop.jpg"
+    Front.src = "images/home/front.jpg"
+    Eyelet.src = "images/home/eyelets.jpg"
+    Fastener.src = "images/home/fasteners.jpg"
+    Rope.src = "images/home/rope.jpg"
+    Wire.src = "images/home/wire.jpg"
+    Security.src = "images/home/security.jpg"
+    Garage.src = "images/home/garage.jpg"
+    Pop.src = "images/home/pop.jpg"
 }
 else {
     Front = ""
